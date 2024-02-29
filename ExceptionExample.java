9df74b0ecfe9f2bf51972c2939f232e06989c719
soulnote <dataouter@gmail.com>
2024-02-29 20:16:54 +0530

    writing simple java code

--- ExceptionExample.java
+++ ExceptionExample.java
@@ -1,1 +1,13 @@
 
+public class ExceptionExample {
+
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+
+        System.out.println("we have got two numbers");
+        int result =  n1/n2;
+        System.out.println("division is "+result);
+        System.out.println("code terminated...");
+    }
+}
